65da83576b5b75b6fb0fc06bf3f1af50f8faa7ef
Zack <zack.zeroliquid@gmail.com>
2023-03-17 23:52:23 +0500

    feat: update beneficiary addresses in deployment script

--- script/Deployment.s.sol
+++ script/Deployment.s.sol
@@ -13,14 +13,14 @@ contract Deployment is Script {
     ZeroLiquidIncentiveVesting internal incentiveVesting;
     ZeroLiquidCoreContributorsVesting internal contributorsVesting;
 
-    address internal developmentBeneficiaryAddress = 0x93acb21dA38Dd48396F0a46cCa6D4d406164244a;
-    address internal governanceBeneficiaryAddress = 0x57b4aEE2c9aA08524df28967ef37eA0555d23D87;
-    address internal incentiveBeneficiaryAddress = 0x8CF3D38f5bF2Fef18a161bDc8a1967631Bcc7164;
-    address internal contributorsBeneficiaryAddress = 0x322AA4997A185242C51a9570eBD7dc330de2d4A9;
+    address internal developmentBeneficiaryAddress = 0x6fF9474923510C0D41d246b9f39259cbf4E5ebA3;
+    address internal governanceBeneficiaryAddress = 0xAF8794cDA6Aa82e7E0589B0684a24A47C161f9e2;
+    address internal incentiveBeneficiaryAddress = 0x5B9Dd9C8e2DDfb91Ed1a6DFCf2A0A0605e2F6BF3;
+    address internal contributorsBeneficiaryAddress = 0x9FA1B904ba1E29ed45E183EFE6a47aCDC2d15eFA;
 
     uint256 internal developmentCliffDuration = 7_776_000;
     uint256 internal governanceCliffDuration = 15_552_000;
-    uint256 internal incentiveCliffDuration = 15_552_000;
+    uint256 internal incentiveCliffDuration = 2_592_000;
     uint256 internal contributorsCliffDuration = 15_552_000;
 
     uint256 internal developmentVestingDuration = 93_312_000;
